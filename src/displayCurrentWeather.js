fdbd3738de9be1120859d458dc8e77b7d63ebe18
Dvir <dvir1225@gmail.com>
2022-10-06 21:49:09 +0300

    replace appendText with display weather + forecast

---
+++ src/displayCurrentWeather.js
@@ -0,0 +1,42 @@
+import getTime from "./getTime";
+
+const displayCurrentWeather = (weather) => {
+    const weatherDiv = document.querySelector('.weatherDiv');
+    const visibility = weather.visibility;
+    const getVisibility = () => {
+        let result;
+        if(visibility<40){
+            result = 'Dense fog'
+        } else if(visibility<200){
+            result = 'Thick fog'
+        }else if(visibility<400){
+            result = 'Fog'
+        } else if(visibility<1000){
+            result = 'Moderate fog'
+        } else if(visibility<2000){
+            result = 'Very poor visibility'
+        } else if(visibility<4000){
+            result = 'Poor visibility'
+        } else if(visibility<9000){
+            result = 'Moderate visibility'
+        } else if(visibility<20000){
+            result = 'Good visibility'
+        } else if(visibility<40000){
+            result = 'Very good visibility'
+        } else {
+            result = 'Excellent visibility'
+        }
+        return result;
+    }
+
+    weatherDiv.innerHTML = `
+    <div class="placeName">${weather.name}, ${weather.country}</div>
+    <div class="timeDate">${getTime(weather)}</div>
+    <div class="currentTemp">Current temp is ${weather.current}°C</div>
+    <div class="feelsLike">Feels like ${weather.feelsLike} °C</div>
+    <div class="visibility">${getVisibility()}</div>`;
+
+    setInterval(function (){document.querySelector('.timeDate').innerText = getTime(weather)}, 1000);
+}   
+
+export default displayCurrentWeather
--- src/getWeatherData.js
+++ src/getWeatherData.js
@@ -1,18 +1,20 @@
-async function getWeatherData(place){
+export default async function getWeatherData(place){
     const getWeatherData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&APPID=c20e51e1cd4bf0147fee570dd34d8dd1&units=metric&exclude=minutely,hourly,daily,alerts`)
     const parsedData = await getWeatherData.json()
+    console.log(parsedData)
     let weather = {
         name: parsedData.name,
-        current: Math.floor(parsedData.main.temp),
-        feelsLike: Math.floor(parsedData.main.feels_like),
-        minTemp: Math.floor(parsedData.main.temp_min),
-        maxTemp: Math.floor(parsedData.main.temp_max),
+        country: parsedData.sys.country,
+        current: Math.round((parsedData.main.temp)*10)/10,
+        feelsLike: Math.round((parsedData.main.feels_like)*10)/10,
         weatherMain: parsedData.weather[0].main,
         weatherDescription: parsedData.weather[0].description,
+        icon: parsedData.weather[0].icon,
+        visibility: parsedData.visibility,
         humidity: parsedData.main.humidity,
+        wind: parsedData.wind.speed,
         offset: parsedData.timezone
     };
     return weather
 }
 
-export default getWeatherData;
